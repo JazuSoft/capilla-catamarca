1ce19d56f8c2a92e82777621cd4a10c338881fd1
Ezequiel <falezekiel@gmail.com>
2023-07-26 17:07:08 -0300

    mvp5

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  year: any = ''
+  constructor(private datePipe: DatePipe) { }
+  ngOnInit(): void {
+    this.year = this.getCurrentYear();
+  }
 
-  anio: any = ''
+  getCurrentYear(): number {
+    const currentDate = new Date();
+    const formattedDate = this.datePipe.transform(currentDate, 'yyyy')!;
+    return parseInt(formattedDate, 10);
+  }
 
 }
